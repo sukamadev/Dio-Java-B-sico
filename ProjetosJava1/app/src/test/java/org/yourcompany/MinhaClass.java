b09845151c56987003d8183fb5de39711c96e879
sukama <sukamasarmento@gmail.com>
2024-05-03 03:29:31 -0300

    Anatomia ds clssses

---
+++ ProjetosJava1/app/src/test/java/org/yourcompany/MinhaClass.java
@@ -0,0 +1,15 @@
+package org.yourcompany;
+
+public class MinhaClass {
+    public static void main (String [] args) {
+        String primeiroNome = "Sukama";
+        String seegundoNome = "Sarmento";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, seegundoNome);
+        System.out.print(nomeCompleto);
+
+    }
+    public static String nomeCompleto (String primeiroNome, String sugundoNome) {
+        return "Resuldado do método: " + primeiroNome.concat(" ").concat(sugundoNome);
+    }
+}
